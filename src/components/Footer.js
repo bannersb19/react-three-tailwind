ec627029ceff7460a94c6ac99279a6b5f5a2ac8c
Sujash Banerjee <sujashbanerjee@gmail.com>
2023-04-28 20:03:19 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export const Footer = () => {
+   
+    return (
+        <Footer className="absolute bottom-1 w-full flex justify-center">
+            <p className="text-[#ff0a65]">&copy;Code and Play</p>
+        </Footer>
+    )
+}
+
+// export default Footer;
